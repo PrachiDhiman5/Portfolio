5011cf1c6bace3ac535c91a9633efe1907db27eb
PrachiDhiman5 <prachidhiman362@gmail.com>
2026-03-21 00:02:10 +0530

    added

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 5000;
 // Nodemailer Setup
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
